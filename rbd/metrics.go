5a1fb10e181e7f117be2399785ff8349fa5df3ac
YuYang <yyangplus@gmail.com>
2023-10-16 14:16:11 +0800

    remove metrics

--- rbd/metrics.go
+++ rbd/metrics.go
@@ -2,9 +2,6 @@ package rbd
 
 import (
 	"context"
-	"fmt"
-	"math"
-	"strings"
 
 	"github.com/mitchellh/mapstructure"
 	plugintypes "github.com/projecteru2/core/resource/plugins/types"
@@ -13,38 +10,26 @@ import (
 // GetMetricsDescription .
 func (p Plugin) GetMetricsDescription(context.Context) (*plugintypes.GetMetricsDescriptionResponse, error) {
 	resp := &plugintypes.GetMetricsDescriptionResponse{}
-	return resp, mapstructure.Decode([]map[string]interface{}{
-		{
-			"name":   "rbd_capacity",
-			"help":   "node available rbd.",
-			"type":   "gauge",
-			"labels": []string{"podname", "nodename"},
-		},
-		{
-			"name":   "rbd_used",
-			"help":   "node used rbd.",
-			"type":   "gauge",
-			"labels": []string{"podname", "nodename"},
-		},
+	return resp, mapstructure.Decode([]map[string]any{
+		// {
+		// 	"name":   "rbd_used",
+		// 	"help":   "node used rbd.",
+		// 	"type":   "gauge",
+		// 	"labels": []string{"podname", "nodename"},
+		// },
 	}, resp)
 }
 
 // GetMetrics .
 func (p Plugin) GetMetrics(ctx context.Context, podname, nodename string) (*plugintypes.GetMetricsResponse, error) { //nolint
-	safeNodename := strings.ReplaceAll(nodename, ".", "_")
-	metrics := []map[string]interface{}{
-		{
-			"name":   "rbd_capacity",
-			"labels": []string{podname, nodename},
-			"value":  fmt.Sprintf("%+v", math.MaxInt64),
-			"key":    fmt.Sprintf("core.node.%s.rbd.capacity", safeNodename),
-		},
-		{
-			"name":   "rbd_used",
-			"labels": []string{podname, nodename},
-			"value":  fmt.Sprintf("%+v", 0),
-			"key":    fmt.Sprintf("core.node.%s.rbd.used", safeNodename),
-		},
+	// safeNodename := strings.ReplaceAll(nodename, ".", "_")
+	metrics := []map[string]any{
+		// {
+		// 	"name":   "rbd_used",
+		// 	"labels": []string{podname, nodename},
+		// 	"value":  fmt.Sprintf("%+v", 0),
+		// 	"key":    fmt.Sprintf("core.node.%s.rbd.used", safeNodename),
+		// },
 	}
 
 	resp := &plugintypes.GetMetricsResponse{}
